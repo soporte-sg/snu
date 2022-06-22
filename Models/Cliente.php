d6a4f7c1e393bfa31e67abefe01028db18552f8c
calidadgrep <alexsanderorejuela@gmail.com>
2022-06-22 12:55:27 -0500

    cleintes

--- Models/Cliente.php
+++ Models/Cliente.php
@@ -3,6 +3,22 @@ require_once 'App.php';
 
 class Cliente
 {
+
+    private $pdo;
+    public $id;
+    public $nombre;
+    public $telefono;
+    public $correos;
+    public $direccion;
+    public $salario;
+    public $matriz;
+    public $fechainicio;
+    public $rector;
+    public $rect_telefono;
+    public $filename;
+    public $dir;
+
+
     public function __CONSTRUCT()
     {
         try {
@@ -23,7 +39,60 @@ class Cliente
                                ORDER BY clientes.id ");
             $stm->execute();
             return $stm->fetchAll(PDO::FETCH_OBJ);
-        } catch (\Throwable $th) {
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function upCliente($id)
+    {
+
+        try {
+
+            $stm = $this->pdo->prepare("SELECT * FROM  clientes WHERE id='$id'");
+            $stm->execute();
+            return $stm->fetch(PDO::FETCH_OBJ);
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function Registrar(Cliente $data)
+    {
+
+        try {
+
+            $stm = "INSERT INTO clientes(nombre, direccion, telefono, correos, salario, matriz, fechainicio, rector, rect_telefono, filename,dir )
+            VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?)";
+            $this->pdo->prepare($stm)->execute(array(
+                $data->nombre,
+                $data->direccion,
+                $data->telefono,
+                $data->correos,
+                $data->salario,
+                $data->matriz,
+                $data->fechainicio,
+                $data->rector,
+                $data->rector,
+                $data->filename='--',
+                $data->dir='--'
+            ));
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public function Actualizar(Cliente $data)
+    {
+
+        try {
+
+            $sql = "UPDATE clientes SET nombre='$data->nombre', direccion='$data->direccion', telefono='$data->telefono',
+              correos='$data->correos', salario='$data->salario', matriz='$data->matriz', fechainicio='$data->fechainicio',
+              rector='$data->rector'  WHERE id = $data->id";
+            $this->pdo->prepare($sql)->execute();
+        } catch (Exception $e) {
+            die($e->getMessage());
         }
     }
 }
